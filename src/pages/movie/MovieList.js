f536e85cd2c37899758eb92f0380199fbfae21b6
jearjujaroen <jear.jujaroen@gmail.com>
2020-06-07 16:02:42 -0400

    feat(MovieList): add MovieList file

---
+++ src/pages/movie/MovieList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from './MovieList.module.css';
+
+import { LazyImage } from '../../components/LazyImage/Image';
+
+export const MovieList = ({ movies, basePosterUrl }) => {
+  return (
+    <div className={styles.container}>
+      {movies.length > 0 ? (
+        <ol className={styles.wrapper}>
+          {movies.map(movie => (
+            <li className={styles.box} key={movie.id}>
+              {movie.poster_path && (
+                <div className={styles.cardContainer}>
+                  <Link to={`/movie/${movie.id}`}>
+                    <LazyImage src={`${basePosterUrl}${movie.poster_path}`} />
+                    <div className={styles.descriptionText}>
+                      <p>{movie.vote_average}</p>
+                    </div>
+                  </Link>
+                </div>
+              )}
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p>No results!</p>
+      )}
+    </div>
+  );
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.array.isRequired,
+  basePosterUrl: PropTypes.string.isRequired,
+};
